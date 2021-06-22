cfa51f1c377771b14536f2ec9a6aa2e6f11bcdd0
Victor Cubiles <victordnb@gmail.com>
2021-06-22 22:55:08 +0200

    This is a database connect, but it need to fix and check

--- src/index.js
+++ src/index.js
@@ -3,19 +3,21 @@ require('express-async-errors');
 const cors = require('cors');
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 
 // const ProductRouter = require('./controllers/ProductRouter');
 
 const AuthRouter = require('./controllers/AuthRouter');
 
-app.use(cors({
-    origin: '*',
-    optionsSuccessStatus: 200,
-}));
+app.use(
+    cors({
+        origin: '*',
+        optionsSuccessStatus: 200,
+    })
+);
 
-app.use(express.json())
+app.use(express.json());
 // app.use(AuthRouter);
 
 app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
