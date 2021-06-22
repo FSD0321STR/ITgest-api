1d07628d1690627f29633198dc23c65f68f2817a
Victor Cubiles <victordnb@gmail.com>
2021-06-22 23:09:19 +0200

    This is a database connect, but it need to fix and check

--- src/models/mongoose.js
+++ src/models/mongoose.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect(`mongodb+srv://dbITgestion:${process.env.MONGODB_PASSWORD}@cluster0.seyh6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true }, (err, res) => {
+mongoose.connect(`mongodb+srv://dbITgestion:${process.env.MONGODB_PASSWORD}@cluster0.seyh6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
