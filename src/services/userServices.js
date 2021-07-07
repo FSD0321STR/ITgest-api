91d8b28e8b89b91a0ddd6a74d87fea174df8a183
Victor Cubiles <victordnb@gmail.com>
2021-07-07 23:31:20 +0200

    adding userList + validations , fix bugs with usersSWervices, no finish

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -3,7 +3,12 @@ const create = (fields) => new User(fields).save();
 const exists = (id) => User.exists({ _id: id });
 const findByEmail = (email) => User.findOne({ email });
 const findById = (id) => User.findById(id);
-const Users  = () =>  User.find();
+const mongoose = require('mongoose');
+const { Provider } = require('../models/provider');
+
+const Users = async () => {
+  return await Provider.find().populate('users').exec();
+};
 
 module.exports = {
   create,
@@ -11,4 +16,4 @@ module.exports = {
   findByEmail,
   findById,
   Users,
-};
+}
