f04b971d862cc9ae7e44400933e61a3cf6af88f9
Victor Cubiles <victordnb@gmail.com>
2021-07-12 18:37:06 +0200

    Backend producto, archivo mongoose

--- src/models/mongoose.js
+++ src/models/mongoose.js
@@ -7,6 +7,16 @@ const { encryptPassword } = require('../helpers/password');
 
 mongoose.connect(`mongodb+srv://admin:mongoadmin@itgestcluster.1piwo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const Item = mongoose.model('Item', {
+    model: String,
+    Brand: String,
+    Provider: String,
+    Category: String,
+    Price: Number,
+    minStock: Number,
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+});
 
 // SCHEMA USER
 const userSchema = new mongoose.Schema({
@@ -28,6 +38,61 @@ userSchema.pre('save', async function (next) {
 });
 
 const User = mongoose.model('User', userSchema);
+
+const itemCreateSchema = {
+    type: "object",
+    properties: {
+      title: {type: "string"},
+      brand: {type: "string"},
+      provider: {type: "string"},
+      Category: {type: "string"},
+      Price: {type: "number"},
+      minStock: {type: "number"},
+    },
+    required: ["title", "brand", "provider", "Price"],
+    additionalProperties: false
+};
+
+
+const itemUpdateSchema = {
+    type: "object",
+    properties: {
+        title: {type: "string"},
+        brand: {type: "string"},
+        provider: {type: "string"},
+        Category: {type: "string"},
+        Price: {type: "number"},
+        minStock: {type: "number"},
+      },
+    required: [],
+    additionalProperties: false
+};
+
+
+const itemPatchSchema = {
+    type: "object",
+    properties: {
+        title: {type: "string"},
+        brand: {type: "string"},
+        provider: {type: "string"},
+        Category: {type: "string"},
+        Price: {type: "number"},
+        minStock: {type: "number"},
+      },
+    required: [],
+    additionalProperties: false
+};
+
+validateItem = (document, method) => {
+    switch (method) {
+        case 'POST':
+            return ajv.validate(itemCreateSchema, document);
+        case 'PUT':
+            return ajv.validate(itemUpdateSchema, document);;
+        case 'PATCH':
+            return ajv.validate(itemPatchSchema, document);;
+    }
+}
 
 
 
